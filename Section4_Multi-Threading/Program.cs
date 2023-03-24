7c687fa107853cb3137ef4ecb5d928dea889fe49
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-03-24 15:58:32 +0530

    Adding welcome message

---
+++ Section4_Multi-Threading/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section4_Multi_Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the MultiThreading Concept");
+        }
+    }
+}
